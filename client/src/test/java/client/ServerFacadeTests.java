defb586d770f0bd902e0750125767c136137e6b0
Nathan Longhurst <nathanlonghurst@gmail.com>
2024-06-15 13:42:21 -0600

    updated for tests

--- client/src/test/java/client/ServerFacadeTests.java
+++ client/src/test/java/client/ServerFacadeTests.java
@@ -19,7 +19,7 @@ public class ServerFacadeTests {
         server = new Server();
         var port = server.run(8080);
         System.out.println("Started test HTTP server on " + port);
-        facade = new ServerFacade("http://localhost:8080");
+        facade = new ServerFacade("http://localhost:0");
     }
 
     @AfterAll
